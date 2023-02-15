02bd80f6fc7c067e8130060defdd70d1aaafb046
Zapplightforce <pers0010@hz.nl>
2023-02-15 18:28:35 +0100

    Create Pepperoni pizza

---
+++ NYStylePepperoniPizza.java
@@ -0,0 +1,9 @@
+class NYStylePepperoniPizza extends Pizza {
+    public NYStylePepperoniPizza() {
+        description = "NY Style Pepperoni Pizza";
+    }
+
+    public double cost() {
+        return 15.99;
+    }
+}
